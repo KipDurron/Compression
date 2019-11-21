50eef85b199c210fc48be871231fb5d77503df5a
kipdurron <ishunin97@gmail.com>
2019-11-22 00:02:02 +0300

    created one compression

--- js/AlhgoritmZeroOrder.js
+++ js/AlhgoritmZeroOrder.js
@@ -1,81 +1,73 @@
+// import  show_graph_2  from './AlhgoritmOneOrder';
+
 $(document).ready(function () {
     $('#submit-file').on("click",function(e){
         e.preventDefault();
         $('#files').parse({
             config: {
                 delimiter: "auto",
-                complete: show_graph,
-            },
-            before: function(file, inputElem)
-            {
-                //console.log("Parsing file...", file);
-            },
-            error: function(err, file)
-            {
-                //console.log("ERROR:", err, file);
-            },
-            complete: function()
-            {
-                //console.log("Done with all files");
+                complete:  show_graph,
             }
         });
     });
 });
 
 function check_criterion(f_with_star, f){
-    var eps = 0.5;
-    // if () {
-    //     f_with_star - f
-    // }
+    var eps = 5;
+    if (Math.abs((f_with_star - f)) <= eps) {
+        return true;
+    } else {
+        console.log(false);
+        return false;
+    }
+}
+
+function parse_result(data) {
+    var f_with_star = data[0].join(",").split(";")[1];
+    var t_temp = data[0].join(",").split(";")[0];
+    var return_results = [[], []];//первый массив исходные дапнные, второй сжатые
+    return_results[1].push([t_temp, f_with_star]);
+    return_results[0].push(data[0].join(",").split(";"));
+    var f_temp;
+    for(i=1;i<data.length;i++){
+        var row = data[i];
+        var cells = row.join(",").split(";");
+        t_temp = cells[0];
+        f_temp = cells[1];
+        if (!check_criterion(f_with_star, f_temp)) {
+           console.log(f_with_star, f_temp);
+            f_with_star = f_temp;
+        }
+        return_results[1].push([t_temp, f_with_star]);
+        return_results[0].push(cells);//исходные данные
+
+    }
+    return return_results;
 }
 
 function show_graph(results) {
     // данные для графиков
-    var data = results.data;
-    var criterion_fidelity =
-    console.log(data)
+    var data = parse_result(results.data);
     var all_data = [
-        { data: [["2010/10/01", 0], ["2010/10/5", 1],
-                ["2010/10/10", 7], ["2010/10/15", 8]]},
-        { data: [["2010/10/01", 13], ["2010/10/5", 23],
-                ["2010/10/10", 32], ["2010/10/15", 33]]}
+        { data: data[0], label: "True data"},
+        { data: data[1], label: "Compression data"}
     ];
     var options = {
         axisLabels: {
             show: true
         },
         xaxes: [{
-            axisLabel: 'foo',
+            axisLabel: 'Time',
         }],
         yaxes: [{
             position: 'left',
-            axisLabel: 'bar',
+            axisLabel: 'Value from reactor (Zero compression)',
         }, {
             position: 'right',
             axisLabel: 'bleem'
         }]
     };
-// выводим график
-    $.plot($("#placeholder"), [ {data: [[0, 0], [1, 1]], label: "Foo" }], options);
-    var table = "<table class='table'>";
-    var data = results.data;
-
-    for(i=0;i<data.length;i++){
-        table+= "<tr>";
-        var row = data[i];
-        console.log(row)
-        var cells = row.join(" ").split(",");
-        console.log(cells)
-
-        for(j=0;j<cells.length;j++){
-            table+= "<td>";
-            table+= cells[j];
-            table+= "</th>";
-        }
-        table+= "</tr>";
-    }
-    table+= "</table>";
-    $("#parsed_csv_list").html(table);
+    $.plot($("#zero_graph"), all_data, options);
 }
 
 
